f82a48080582f42de3850714619fd500213b6492
Aakarsh Rajagopalan <51793634+rajagoah@users.noreply.github.com>
2020-05-11 17:00:00 -0400

    Code commit
    code bit contains section that loads csv from web and converts to df without saving to local

--- ImportingCVSFromWeb.py
+++ ImportingCVSFromWeb.py
@@ -1,10 +1,13 @@
 import pandas as pd
 from urllib.request import urlretrieve
 
+#this code will load file from web and store them in local
 url = "https://s3.amazonaws.com/assets.datacamp.com/production/course_1606/datasets/winequality-red.csv"
-
 urlretrieve(url, 'winequality-red.csv')
-
 df = pd.read_csv('winequality-red.csv',';')
+print(df.head())
 
+#this code will load file from web and convert to dataframe without saving the csv to local
+url = "https://s3.amazonaws.com/assets.datacamp.com/production/course_1606/datasets/winequality-white.csv"
+df = pd.read_csv(url,";")
 print(df.head())
